39be9315c0b34d8c2018dffb790493899836539b
PdskDev <prestadesk@gmail.com>
2023-02-25 19:23:36 +0100

    Step-3 : expiration date validation control

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,19 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { FormArray, FormBuilder, FormGroup } from "@angular/forms";
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
+
+function minDateValidation(date: Date): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const forbidden = new Date(control.value) < date;
+    return forbidden ? { minDateValidation: { value: control.value } } : null;
+  };
+}
 
 @Component({
   selector: "in-product",
@@ -13,13 +27,19 @@ export class ProductComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.productForm = this.fb.group({
       basic: fb.group({
-        name: "",
+        name: ["", Validators.required],
         description: "",
         active: false,
         features: fb.array([fb.control("")]),
       }),
       expiration: fb.group({
-        expirationDate: null,
+        expirationDate: [
+          null,
+          Validators.compose([
+            Validators.required,
+            minDateValidation(new Date()),
+          ]),
+        ],
       }),
     });
   }
@@ -56,5 +76,22 @@ export class ProductComponent implements OnInit {
   addFeature() {
     this.basicFeatures.push(this.fb.control(""));
   }
+
+  get expirationError() {
+    if (
+      this.productForm.get("expiration.expirationDate").hasError("required")
+    ) {
+      return "This field is required";
+    }
+
+    if (
+      this.productForm
+        .get("expiration.expirationDate")
+        .hasError("minDateValidation")
+    ) {
+      return "Expiration should be after today's date";
+    }
+  }
+
   ngOnInit() {}
 }
